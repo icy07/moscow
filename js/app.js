748cc748a810120892268fb1fcc4c84db39369df
icy07 <69872542+icy07@users.noreply.github.com>
2022-08-20 19:16:58 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2228,9 +2228,15 @@
                 let vhBig = .01 * window.innerHeight;
                 document.documentElement.style.setProperty("--vh", `${vhBig + .01}px`);
             }));
+            let span1 = document.querySelector(".test__span1");
+            let span2 = document.querySelector(".test__span2");
+            span1.innerHTML = vh[0].offsetHeight;
+            span2.innerHTML = window.screen.height;
             document.addEventListener("scroll", (function() {
                 if (document.documentElement.clientWidth < 1051 || document.documentElement.clientHeight < 611) {
                     const height = window.screen.height;
+                    span1.innerHTML = vh[0].offsetHeight;
+                    span2.innerHTML = window.screen.height;
                     if (!(vh[0].offsetHeight < height)) for (let i = 0; i < vh.length - 1; i++) {
                         const vhTop = vh[i + 1].getBoundingClientRect().top;
                         const newPadding = Math.ceil((vh[i].offsetHeight - vh[i + 1].offsetHeight) / 2);
