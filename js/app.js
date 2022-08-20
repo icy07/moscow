bd2d916333877693dba7b98181abe12dfdcf8b6a
icy07 <69872542+icy07@users.noreply.github.com>
2022-08-20 13:13:00 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2223,7 +2223,7 @@
             if (document.documentElement.clientWidth < 1051 || document.documentElement.clientHeight < 611) {
                 const vh = document.querySelectorAll(".vh");
                 const height = document.documentElement.clientHeight;
-                for (let i = 0; i < vh.length - 1; i++) {
+                if (!(vh[0].offsetHeight < height)) for (let i = 0; i < vh.length - 1; i++) {
                     const vhTop = vh[i + 1].getBoundingClientRect().top;
                     const newPadding = Math.ceil((vh[i].offsetHeight - vh[i + 1].offsetHeight) / 2);
                     if (vh[i].offsetHeight > vh[i + 1].offsetHeight) {
@@ -2239,7 +2239,9 @@
                         vh[i + 1].classList.remove("secBlock");
                         vh[i + 1].style.marginTop = `0px`;
                     }
-                }
+                } else vh.forEach((e => {
+                    e.classList.add("bigVH");
+                }));
             }
         }));
     };
