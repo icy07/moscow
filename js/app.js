0b2f299489339103a2531b5e92669227a2f48442
icy07 <69872542+icy07@users.noreply.github.com>
2022-08-22 12:11:27 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2223,11 +2223,6 @@
                 header.classList.toggle("white", posTop - headerHeight <= 0);
             }));
             const vh = document.querySelectorAll(".vh");
-            let span1 = document.querySelector(".test__span1");
-            let span2 = document.querySelector(".test__span2");
-            let span3 = document.querySelector(".test__span3");
-            let span4 = document.querySelector(".test__span4");
-            let span5 = document.querySelector(".test__span5");
             if (document.documentElement.clientWidth < 1051 || document.documentElement.clientHeight < 611) {
                 if (vh[0].offsetHeight < window.screen.height) vh.forEach((e => {
                     e.classList.add("vhBig");
@@ -2238,20 +2233,10 @@
                     fixedBlocks(vh, window.screen.height);
                     console.log("res");
                 }
-                span1.innerHTML = vh[0].offsetHeight;
-                span2.innerHTML = window.screen.height;
-                span3.innerHTML = window.innerHeight;
-                span4.innerHTML = window.pageYOffset;
-                span5.innerHTML = vh[1].style.marginTop;
             }
             document.addEventListener("scroll", (function() {
                 if (document.documentElement.clientWidth < 1051 || document.documentElement.clientHeight < 611) {
                     const height = window.screen.height;
-                    span1.innerHTML = vh[0].offsetHeight;
-                    span2.innerHTML = window.screen.height;
-                    span3.innerHTML = window.innerHeight;
-                    span4.innerHTML = window.pageYOffset;
-                    span5.innerHTML = vh[1].style.marginTop;
                     if (!(vh[0].offsetHeight < height) && !vh[0].classList.contains("vhBig")) fixedBlocks(vh, height); else vh.forEach((e => {
                         e.classList.add("vhBig");
                         let vhBig = .01 * window.innerHeight;
